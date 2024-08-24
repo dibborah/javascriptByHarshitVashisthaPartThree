2ce00ece53b15f6b75891b4edbafd68422682465
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-24 18:46:46 +0530

    starting Intro to promises

---
+++ 134.js
@@ -0,0 +1,10 @@
+
+// Intro to promises: 
+
+// I.promise: initially: 
+// 1. status: pending. 2. value: undefined
+
+// II.promises: after execution(afterwards)
+// 1. status: fullfilled. 2. value: Some value
+
+const promise = ['coffee', 'chips', 'vegetables', 'salt', 'rice'];
