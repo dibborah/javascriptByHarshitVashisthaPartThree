75f4035ff2ed544c0a5c98fa0b7334589de5dbf8
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-24 19:06:42 +0530

    done producing a promise

--- 134.js
+++ 134.js
@@ -13,4 +13,24 @@
 // II.promises: after execution(afterwards)
 // 1. status: rejected. 2. value: any Error value
 
-const promise = ['coffee', 'chips', 'vegetables', 'salt', 'rice'];
+const bucket = ['coffee', 'chips', 'vegetables', 'salt', 'rice'];
+
+// 1st part: Producing a promise
+
+// creating or constructing a promise
+// The callback fc inside the promise with resolve and reject is called "EXECUTOR FUNCTION"
+
+const myFriedRicePromise = new Promise((resolve, reject) => {// The arguement here resolve and reject are methods or functions here which we will call
+    // Promise here is a class or a constructor fc
+    if (bucket.includes('vegetables') && bucket.includes('salt') && bucket.includes('rice')){
+        resolve('Fried Rice');// We can passing any value // Here we passing a string value
+    }else {
+        reject('Something went wrong!!!');
+    }
+});
+
+// console.log(myFriedRicePromise); // A promise is produced
+
+
+// 2nd part : Consuming a promise
+
