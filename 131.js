95836d7fbe2065ed08a750ba57a6d2fee5a84b51
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-24 14:21:41 +0530

    started setIntervak

---
+++ 131.js
@@ -0,0 +1,12 @@
+// setInterval
+
+console.log('script start');
+
+const clearId = setInterval(() => {
+    console.log(Math.random());
+}, 1000);
+
+// clearInterval(clearId);
+
+console.log('script end');
+console.log('script end');
