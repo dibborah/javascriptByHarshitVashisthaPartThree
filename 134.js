bc9e33d474bf59374047c397b229ed768d7187cc
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-24 18:51:57 +0530

    done the basic highlevel overview of promises

--- 134.js
+++ 134.js
@@ -1,10 +1,16 @@
+// Intro to promises:
 
-// Intro to promises: 
+// Simple defination: Future value
 
 // I.promise: initially: 
 // 1. status: pending. 2. value: undefined
 
+// 1st case: When success
 // II.promises: after execution(afterwards)
 // 1. status: fullfilled. 2. value: Some value
+
+// 2nd case: When failure(error)
+// II.promises: after execution(afterwards)
+// 1. status: rejected. 2. value: any Error value
 
 const promise = ['coffee', 'chips', 'vegetables', 'salt', 'rice'];
