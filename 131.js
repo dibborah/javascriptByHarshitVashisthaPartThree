9f182b9fe19756ebcd4769ca054b28e7d7806b45
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-24 14:52:42 +0530

    done small changing background project and also stopping it using setInterval clearInterval and DOM manipulation

--- 131.js
+++ 131.js
@@ -1,12 +1,40 @@
-// setInterval
+// // setInterval
 
-console.log('script start');
+// console.log('script start');
 
-const clearId = setInterval(() => {
-    console.log(Math.random());
-}, 1000);
+// const clearId = setInterval(() => {
+//     let total = 0;
+//     // This for loop is delaying the setInterval code beyond the time we provide as arguement
+//     for(let i = 0; i < 1000000000; i++){
+//         total +=i;
+//     }
+//     console.log(total);
+//     console.log(Math.random());
+// }, 0);// donot blindly follow the time provided // The 0s is only the minimum time delay
 
 // clearInterval(clearId);
 
-console.log('script end');
-console.log('script end');
+// console.log('script end');
+
+
+const body = document.body;
+const button = document.querySelector('button')
+const randomColorGenerator = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const rgb = `rgb(${red}, ${green}, ${blue})`;
+    return rgb;
+}
+
+const rgb = randomColorGenerator();
+
+const clearId = setInterval(() => {
+    body.style.background = randomColorGenerator();
+},1000);
+
+button.addEventListener('click', () => {
+    clearInterval(clearId);
+})
+
