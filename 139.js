a5c00d0e5723763aa4a7687847d7d55c5c85d6b2
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-25 13:38:12 +0530

    done AJAX basic theory

---
+++ 139.js
@@ -0,0 +1,32 @@
+// BASIC THEORY
+
+// AJAX: Asynchronous Javascript and XML
+
+// is a set of web development techniques
+// using many web technologies on the 'client side'
+// to create asynchronous web applications
+
+// HTTP request
+// client and server : HTTP madhyam ke jariye baat karte hain ek dusre se
+// Request server
+
+
+// with AJAX, web applications can send and retrieve
+// data from a server asynchronously (in the background)
+// without interfering with the display and
+// behaviour of the existing page
+
+// we donot use XML data format anymore
+// we use JSON data format now
+
+// we have 3 most common ways to create and send request to servers
+// 1. xmlHTTPRequest(old way of doing)
+// fast but too much syntax(long code)
+// 2. fetch API(provided by the browser)(a new way of doing)
+// 3. axios(third party library)
+
+// Note: BTS fetch API and axios also works as xmlHTTPRequest
+// And that's the reason they reduce the line of code and achieve the same result
+
+
+
