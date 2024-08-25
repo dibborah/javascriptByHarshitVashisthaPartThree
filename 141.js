415edf72d3a1a6f8e3dfe036ecebcff4bc9a0b11
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-25 15:53:55 +0530

    done XHR practice and error handling

--- 141.js
+++ 141.js
@@ -7,11 +7,39 @@ const xhr = new XMLHttpRequest();// XMLHttpRequest is a class (constructor funct
 // ----- 1st request -----
 xhr.open('GET', URL);
 
-xhr.onload = function() {
+xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300) {
         const data = JSON.parse(xhr.response);
         console.log(data)
-    }else {
+
+        // 2nd request inside the first request // May be a dependent request
+
+        const id = data[3]?.id;
+
+
+        const xhr1 = new XMLHttpRequest(); // XMLHttpRequest is a class (constructor function)
+
+        const URL2 = `${URL}/${id}`;
+
+        console.log(URL2);
+
+        xhr1.open('GET', URL2);
+
+        xhr1.onload = function () {
+            if (xhr1.status >= 200 && xhr1.status < 300) {
+                const data2 = JSON.parse(xhr1.response);
+                console.log(data2)
+            } else {
+                console.log('invalid request');
+            }
+        };
+
+        xhr1.onerror = () => {
+            console.log('Network error');
+        }
+        xhr1.send();
+
+    } else {
         console.log('invalid request');
     }
 };
@@ -24,20 +52,20 @@ xhr.send();
 
 // ----- 2nd request ------
 
-const xhr1 = new XMLHttpRequest();// XMLHttpRequest is a class (constructor function)
+// const xhr1 = new XMLHttpRequest();// XMLHttpRequest is a class (constructor function)
 
-xhr1.open('GET', `${URL}/1`);
+// xhr1.open('GET', `${URL}/1`);
 
-xhr1.onload = function() {
-    if (xhr1.status >= 200 && xhr1.status < 300) {
-        const data = JSON.parse(xhr1.response);
-        console.log(data)
-    }else {
-        console.log('invalid request');
-    }
-};
+// xhr1.onload = function() {
+//     if (xhr1.status >= 200 && xhr1.status < 300) {
+//         const data = JSON.parse(xhr1.response);
+//         console.log(data)
+//     }else {
+//         console.log('invalid request');
+//     }
+// };
 
-xhr1.onerror = () => {
-    console.log('Network error');
-}
-xhr1.send();
+// xhr1.onerror = () => {
+//     console.log('Network error');
+// }
+// xhr1.send();
