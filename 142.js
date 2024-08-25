d4fc6d668ed2a09f72d0b3fbd4e9720d999bb647
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-25 16:41:24 +0530

    done xhr using promises

---
+++ 142.js
@@ -0,0 +1,70 @@
+// XHR using promises
+
+const getPosts = (method, URL) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', URL);
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                const response = xhr.response;
+                const data = JSON.parse(response);
+                resolve(data);
+            } else {
+                reject('Something went wrong');
+            }
+        };
+        xhr.onerror = () => {
+            console.log('Network error');
+        }
+        xhr.send();
+    })
+};
+
+// Non - refactored code( Not using the powers of promises)
+// getPosts()
+//     .then((data) => {
+//         console.log(data);
+//         return data;
+//     }).then((data) => {
+//         // This then block is something I have doubt off. It could be refactored
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET', `${URL}/4`);
+//         xhr.onload = () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 const response = xhr.response;
+//                 const data = JSON.parse(response);
+//                 console.log(data);
+//             } else {
+//                 console.log('Something went wrong');
+//             }
+//         };
+//         xhr.onerror = () => {
+//             console.log('Network error');
+//         }
+//         xhr.send();
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+
+// Refactored code(using the powers of promises)
+
+const url1 = 'https://jsonplaceholder.typicode.com/posts';
+
+getPosts('GET', url1).then((data) => {
+    console.log(data);
+    return data;
+})
+.then((data) => {
+    const id = data[3]?.id;
+    return id;    
+})
+.then((id) => {
+    const url2 = `${url1}/${id}`;
+    return getPosts('GET', url2);
+})
+.then((data) => {
+    console.log(data);
+})
+.catch(error => {
+    console.log(error);
+})
